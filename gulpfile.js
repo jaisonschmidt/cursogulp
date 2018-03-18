5c1f03bc105428e98c10e029adf184744de5077a
Jaison Schmidt <jaisonschmidt@gmail.com>
2018-03-17 22:40:22 -0300

    Aula 05 e 06 - task default e watch

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,16 @@ gulp.task('html', function(){
     return gulp.src('app/index.html')
     .pipe(gulp.dest('build/'));
 });
+
+
+/**
+ * Task watch
+ */
+gulp.task('watch', function(){
+    gulp.watch('app/index.html', ['html']);
+});
+
+/**
+ * Task Default
+ */
+gulp.task('default', ['html', 'watch']);
