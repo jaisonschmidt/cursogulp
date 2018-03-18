aab5db91b50afd2599ccfc7ee26899ce7a193651
Jaison Schmidt <jaisonschmidt@gmail.com>
2018-03-18 13:38:49 -0300

    Aula 08 - plugin gulp concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 
+var concat = require('gulp-concat');
+
 // copiar o arquivo index.html da pasta app para a pasta build
 
 /**
@@ -8,6 +10,15 @@ var gulp = require('gulp');
 gulp.task('html', function(){
     return gulp.src('app/index.html')
     .pipe(gulp.dest('build/'));
+});
+
+/**
+ * CSS
+ */
+gulp.task('css', function(){
+    return gulp.src('app/css/*.css')
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('build/css/'))
 });
 
 
