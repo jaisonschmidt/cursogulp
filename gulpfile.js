003e5fce19cdd5b0483db0cfa709b77df1165a65
Jaison Schmidt <jaisonschmidt@gmail.com>
2018-03-18 22:58:51 -0300

    Aula 12 - gulp autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
 var cssBeautify = require('gulp-cssbeautify');
+var autoprefixer = require('gulp-autoprefixer');
 
 var cssFiles = ['app/css/core.css', 
                 'app/css/grid.css', 
@@ -27,6 +28,7 @@ gulp.task('css', function(){
     .pipe(concat('style.min.css'))
     //.pipe(cleanCSS())
     .pipe(cssBeautify())
+    .pipe(autoprefixer())
     .pipe(gulp.dest('build/css/'))
 });
 
